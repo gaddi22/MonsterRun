c27f361d8976a2b34b135925a9aeb46fa6c84f9e
gadmorales <gmorales@dbsgroup.net>
2021-10-27 15:46:41 -0500

    UI Branch Changes

--- app/src/main/java/com/swoopsoft/monsterrun/model/Player.java
+++ app/src/main/java/com/swoopsoft/monsterrun/model/Player.java
@@ -1,26 +1,40 @@
 package com.swoopsoft.monsterrun.model;
 
+import androidx.annotation.NonNull;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.firebase.ui.*;
+import com.google.firebase.database.Query;
 
 public class Player {
     private DatabaseReference userRef;
 
-    public HashMap<String, Object> statistics;
+    public Map<String, Object> statistics;
     public String id;
     public String email;
     public String username;
     public double money;
 
-    public Player(HashMap statistics, String id, String email, String username, double money){
+    public List<Leaderboard> leaderboards;
+
+    public Player(HashMap statistics, String id, String email, String username, double money, List<Leaderboard> leaderboards){
         userRef = FirebaseDatabase.getInstance().getReference("players/"+id);
         this.statistics=statistics;
         this.id=id;
         this.email=email;
         this.username=username;
         this.money=money;
+        this.leaderboards = leaderboards;
     }
 
     /*Update and Sync Methods*/
@@ -30,6 +44,33 @@ public class Player {
         userRef.child("email").setValue(email);
         userRef.child("username").setValue(username);
         userRef.child("money").setValue(money);
+        userRef.child("leaderboards").setValue(leaderboards);
+
+    }
+
+    //updates this class with values from the database
+    public void sync(){
+        Query queryRef = userRef.orderByChild("id").equalTo(id).limitToFirst(1);
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Player>()
+                .setQuery(queryRef, new SnapshotParser<Player>(){
+                    @NonNull
+                    @Override
+                    public Player parseSnapshot(@NonNull DataSnapshot snapshot) {
+                        return new Player((HashMap)snapshot.child("statistics").getValue(),
+                                snapshot.child("id").getValue().toString(),
+                                snapshot.child("email").getValue().toString(),
+                                snapshot.child("username").getValue().toString(),
+                                (double)snapshot.child("money").getValue(),
+                                (List)snapshot.child("leaderboards").getValue());
+                    }
+                })
+                .build();
+
+        this.statistics = ((Player) options.getSnapshots().get(0)).statistics;
+        this.id = ((Player) options.getSnapshots().get(0)).id;
+        this.email = ((Player) options.getSnapshots().get(0)).email;
+        this.username = ((Player) options.getSnapshots().get(0)).username;
+        this.money = ((Player) options.getSnapshots().get(0)).money;
 
     }
 }
