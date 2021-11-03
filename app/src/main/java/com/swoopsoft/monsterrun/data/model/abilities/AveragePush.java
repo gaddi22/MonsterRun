a9d61b52edcd53d58fdecc8d634f2e62ea32b273
gadmorales <gmorales@dbsgroup.net>
2021-11-03 16:16:33 -0500

    Added some name and description for two abilities

--- app/src/main/java/com/swoopsoft/monsterrun/data/model/abilities/AveragePush.java
+++ app/src/main/java/com/swoopsoft/monsterrun/data/model/abilities/AveragePush.java
@@ -4,6 +4,9 @@ import com.swoopsoft.monsterrun.data.model.Monster;
 
 public class AveragePush extends Ability {
 
+    public final String description = "Pushes a monster 400 steps back";
+    public final String name = "Average Push";
+
     @Override
     public void ability(){
         Monster.distance += 400;
--- app/src/main/java/com/swoopsoft/monsterrun/data/model/abilities/WeakPush.java
+++ app/src/main/java/com/swoopsoft/monsterrun/data/model/abilities/WeakPush.java
@@ -1,7 +1,8 @@
 package com.swoopsoft.monsterrun.data.model.abilities;
 
 public class WeakPush extends Ability {
-    public String description;
+    public final String description = "Pushes the monster back 200 steps";
+    public final String name = "Weak Push";
 
     @Override
     public void ability(){
